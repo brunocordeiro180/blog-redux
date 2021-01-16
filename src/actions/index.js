ada3e39d5e99f0e60888a2056700e512a8f03f11
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-01-16 18:58:02 -0300

    fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach(id => dispatch(fetchUser(id)))
+		.value();
+};
+
 
 export const fetchPosts = () => async (dispatch) => {
 
@@ -10,3 +23,22 @@ export const fetchPosts = () => async (dispatch) => {
 	});
 
 }
+
+export const fetchUser = (id) => async (dispatch) => {
+
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+}
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+// 	dispatch({
+// 		type: 'FETCH_USER',
+// 		payload: response.data
+// 	});
+// })
+
